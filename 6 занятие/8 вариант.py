985a5a8fda0a5e8611ea29cb399a199009f8c85e
karina136 <penkina.karina04@bk.ru>
2022-12-09 10:05:09 +0300

    /
    /

--- 6 занятие/8 вариант.py
+++ 6 занятие/8 вариант.py
@@ -1,13 +1,15 @@
 # -- coding: utf-8 --
 s=input("Введите строку \n")
-flag=0
-w=0
-string=""
-for i in range (len(s)):
-    if s[i] != ' ' and flag == 0:
-        w += 1
-        flag = 1
-    else:
-        if s[i] == ' ':
-            flag = 0
-print("Число слов в строке:", w)
+def fun(x):
+    flag=0
+    w=0
+    string=""
+    for i in range (len(x)):
+        if x[i] != ' ' and flag == 0:
+            w += 1
+            flag = 1
+        else:
+            if x[i] == ' ':
+                flag = 0
+    print("Число слов в строке:", w)
+fun(s)
