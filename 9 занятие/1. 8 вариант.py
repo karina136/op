b990a63c01a2470c5f043e842f16166a74d0c46b
karina136 <penkina.karina04@bk.ru>
2022-12-09 10:56:11 +0300

    /
    /

--- 9 занятие/1. 8 вариант.py
+++ 9 занятие/1. 8 вариант.py
@@ -3,23 +3,24 @@ from random import randint
 n=int(input("Введите порядок матрицы n= "))
 print("Исходная матрица:")
 A=[]
-for i in range (n):
-    B=[]
-    for j in range(n):
-        B.append(j)
-    A.append(B)
+def fun(x,y):
+    for i in range (x):
+        B=[]
+        for j in range(x):
+            B.append(j)
+        y.append(B)
 
 
-for i in range (n):
-        for j in range(n):
-            A[i][j]=randint(1,20)
+    for i in range (x):
+            for j in range(x):
+                y[i][j]=randint(1,20)
 
 
-for i in range (n):
-        for j in range(n):
-            print(A[i][j], end=' ')
-        print()
-
+    for i in range (x):
+            for j in range(x):
+                print(y[i][j], end=' ')
+            print()
+fun(n,A)
 
 k=int(input("Введите число k="))
 for n in range(0, len(A)):
